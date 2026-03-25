54b7c6e730fc89ee3b8792be992ab443ecb7aa0c
pateldhruvil18 <DHP204600@GMAIL.COM>
2026-03-25 16:36:04 +0530

    Fixed buildErrorObject import in validateRequest

--- backend/api/utils/validateRequest.js
+++ backend/api/utils/validateRequest.js
@@ -1,5 +1,5 @@
 const { validationResult } = require('express-validator');
-const { buildErrorObject } = require('./buildErrorObject');
+const buildErrorObject = require('./buildErrorObject');
 
 exports.validateRequest = (req, res, next) => {
   const errors = validationResult(req);
